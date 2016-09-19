db78303353bf77f18886aae5be336c336167dbb9
Linqi <linqi@waltznetworks.com>
2016-09-18 20:11:24 -0700

    New index file

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 /* jshint node: true */
 'use strict';
 
-var CouchbaseIndexAdapter = require("./lib/couchbase.js");
+var CouchbaseIndexAdapter = require("./lib/couchbase.js"),
+    BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
   name: 'ember-cli-deploy-couchbase'
 
   createDeployPlugin: function(options) {
-    return {
-    };
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name;
+
+      requiredConfig: ['host', 'bucketName'],
+
+      upload: function(context) {
+      }
+    return new DeployPlugin();
   }
 };
